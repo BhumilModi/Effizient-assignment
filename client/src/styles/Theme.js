f7c47a87efe1d320f180593256ef521b03943408
Bhumil Modi <bhumilmodi2002@gmail.com>
2023-09-02 09:16:51 +0530

    Added template for email

--- client/src/styles/Theme.js
+++ client/src/styles/Theme.js
@@ -1,6 +1,9 @@
 import { createTheme } from "@mui/material";
 
 export const customTheme = createTheme({
+  typography: {
+    fontFamily: 'Roboto Condensed, sans-serif', // Replace 'YourCustomFont' with your actual font name
+  },
   components: {
     MuiStack: {
       styleOverrides: {
--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -15,11 +15,25 @@ const sendEmail = async (req, res) => {
     from: EMAIL_SENDER, // Sender email address
     to: req.body.email, // Recipient email address
     cc: "",
-    subject: "Statement of Purpose draft", // Email subject
-    text: `The submitted response is : \n ${emailBody}`, // Email text body
+    templateId: "d-dd1fae65a9924af3b74b1101d6086f2e",
+    dynamicTemplateData: {
+      subject: `Statement of Purpose Response by ${req.body.firstName} ${req.body.lastName}`, // Email subject
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: req.body.age,
+      institute: req.body.institute,
+      course: req.body.course,
+      work: req.body.work,
+      admitted: req.body.admitted,
+      program: req.body.program,
+      country: req.body.country,
+      goals: req.body.goals,
+      feesPaid: req.body.feesPaid || 0,
+      gic: req.body.gic || 0,
+    }
   };
 
-  sendgrid.send(mailOptions, (error, message) => {
+  sendgrid.send(mailOptions, (error) => {
     if (error) {
       res.status(500).json({ message: "Email undelivered" });
     } else {
